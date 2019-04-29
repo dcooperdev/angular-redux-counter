359481c784da409e3160063ee1f40462c3ce73f6
Cooper <dcooper.dev@gmail.com>
2019-04-29 14:03:35 -0300

    items left count in footer

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as filters from 'src/app/filter/filter.action';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
@@ -12,6 +13,7 @@ export class TodoFooterComponent implements OnInit {
 
   public validFilters: filters.ValidTypes[] = ['All', 'Active', 'Completed'];
   public selectedFilter: filters.ValidTypes;
+  public itemsLeft: number;
 
   constructor( private store: Store<AppState> ) { }
 
@@ -19,6 +21,7 @@ export class TodoFooterComponent implements OnInit {
 
     this.store.subscribe(
       (state: any) => {
+        this.countItemsLeft( state.todos );
         this.selectedFilter = state.filter;
       }
     )
@@ -27,6 +30,10 @@ export class TodoFooterComponent implements OnInit {
   changeFilter( filter: filters.ValidTypes ) {
     const action = new filters.SetFilterAction( filter );
     this.store.dispatch( action );
+  }
+
+  countItemsLeft( todos: Todo[] ) {
+    this.itemsLeft = todos.filter( todo => !todo.completed ).length;
   }
 
 }
